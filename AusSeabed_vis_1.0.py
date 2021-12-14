ea71cb66e5e0a0386e3809f5561096f41348bfed
wil9fd <95448591+wil9fd@users.noreply.github.com>
2021-12-15 10:34:02 +1100

    Add files via upload

--- AusSeabed_vis_1.0.py
+++ AusSeabed_vis_1.0.py
@@ -46,7 +46,7 @@ def ausseabed_vis(voyage_input):
     Author: Samson Williams 
     """
     
-    ROOT = "$HOME/mnt/datasets/work/ncmi-gsm/reference/AusSeabed/"
+    ROOT = "$HOME/datasets/work/ncmi-gsm/reference/AusSeabed/"
     BRANCH = "/FP Geotiff"
     INPATH = ROOT + voyage_input + BRANCH
     
