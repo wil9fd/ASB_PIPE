b8ddbe86da6c3b1729744d5a50c1c4e1986b0e89
Williams, Samson (NCMI, Hobart) <Samson.Williams@csiro.au>
2022-06-06 13:21:52 +1000

    Add conda to path

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 mkdir -p miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-x86_64.sh -O miniconda3/miniconda.sh
 bash miniconda3/miniconda.sh -b -u -p miniconda3
+export PATH="~/miniconda3/bin:$PATH"
 rm -rf miniconda3/miniconda.sh
 
 # Initialise conda
